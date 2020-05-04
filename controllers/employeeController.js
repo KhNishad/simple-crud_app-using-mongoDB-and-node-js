de0c9eff0bc32c7d2645ac7ee5752696bff78b74
khNishad <kkhnishad@gmail.com>
2020-05-05 02:23:56 +0600

    edit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -105,7 +105,7 @@ module.exports = router;
 
 
 
-// commenting 
+// commenting the code
 // const express = require('express');
 // var router = express.Router();
 
@@ -148,64 +148,4 @@ module.exports = router;
 //     });
 // }
 
-// // update function
-// function updateRecord(req, res) {
 
-//     employe.findOneAndUpdate({ _id: req.body._id }, { $set: { Name: req.body.name, Contact: req.body.contact, Address: req.body.address } }, { returnOriginal: false }, (err, doc) => {
-
-//         if (!err) {
-
-
-//             res.redirect('employee/list');
-//         }
-//         else {
-//             res.send(err);
-//         }
-//     });
-// }
-
-
-// // retriving data from database and sending in the table
-// router.get('/list', (req, res) => {
-//     employe.find((err, docs) => {
-//         if (!err) {
-
-//             res.render("employee/list.hbs", {
-//                 list: docs
-//             });
-//         } else {
-//             res.send(err);
-
-//         }
-//     }).lean();
-
-// });
-
-// router.get('/:id', (req, res) => {
-//     employe.findById(req.params.id, (err, docs) => {
-//         if (!err) {
-//             //console.log(docs);
-
-//             res.render('employee/addEmployee.hbs', { viewTitle: "Update", employee: docs })
-//         }
-//         else {
-//             console.log("error in find by id");
-
-//         }
-//     }).lean();
-// });
-
-
-// // delete record
-// router.get('/delete/:id', (req, res) => {
-//     employe.findByIdAndRemove(req.params.id, (err, docs) => {
-//         if (!err) {
-//             res.redirect('/employee/list');
-//         } else {
-//             res.send(err);
-//         }
-//     })
-
-// });
-
-// module.exports = router;
